9a086117f4dc69f896c77dd61951b2fa7abc3e02
JoshHerde <jherde97@gmail.com>
2024-01-21 15:06:04 -0500

    reformat test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,5 +4,8 @@ EXAMPLE_DIR="$ROOT_DIR/examples"
 echo $EXAMPLE_DIR
 
 for filename in $EXAMPLE_DIR/*.herd; do
-    /usr/bin/env /usr/lib/jvm/java-11-openjdk-amd64/bin/java -cp /home/jherd/.vscode-server/data/User/workspaceStorage/74afb89b072cc70a269c43c0d638d2bd/redhat.java/jdt_ws/lox_50e34ae4/bin herd.Lox $filename
+    /usr/bin/env \
+    /usr/lib/jvm/java-11-openjdk-amd64/bin/java \
+        -cp /home/jherd/.vscode-server/data/User/workspaceStorage/74afb89b072cc70a269c43c0d638d2bd/redhat.java/jdt_ws/lox_50e34ae4/bin \
+        herd.Lox $filename
 done
